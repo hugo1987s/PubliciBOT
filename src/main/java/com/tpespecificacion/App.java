db7be397deae03e39e55afe7adf03b528bf82f65
Agus Alexander <agusalexander@hotmail.com>
2017-05-06 20:32:17 -0300

    test con maven
    descarga las librerias correctamente

--- src/main/java/com/tpespecificacion/App.java
+++ src/main/java/com/tpespecificacion/App.java
@@ -1,5 +1,7 @@
 package com.tpespecificacion;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
+
+
         System.out.println( "Hello World!" );
+        //Para testear que anda Maven,
+        // lo que esta abajo necesita de librerias
+        // y se deberian bajar solas
+        String p="<p>My paragraph.</p>";
+        System.out.println(StringEscapeUtils.escapeHtml4(p));
     }
 }
