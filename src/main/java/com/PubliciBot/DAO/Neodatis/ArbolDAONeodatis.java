618d3a1dccb5458c29856f14864db7ae75594b7c
Agus Alexander <agusalexander@hotmail.com>
2017-05-22 11:31:03 -0300

    minor fix y borrar clases inutiles

--- src/main/java/com/PubliciBot/DAO/Neodatis/ArbolDAONeodatis.java
+++ src/main/java/com/PubliciBot/DAO/Neodatis/ArbolDAONeodatis.java
@@ -4,9 +4,6 @@ import com.PubliciBot.DAO.Interfaces.ArbolDAO;
 import com.PubliciBot.DM.ArbolTags;
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
-import org.neodatis.odb.Objects;
-import org.neodatis.odb.core.query.IQuery;
-import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 
 import java.util.ArrayList;
 
@@ -19,8 +16,9 @@ public class ArbolDAONeodatis extends DAONeodatis<ArbolTags> implements ArbolDAO
 
 
     public void guardar(ArbolTags arbol) {
-       try {
-           ODB odb = ODBFactory.open(fileNameNeodatisDB);
+        ODB odb =null;
+        try {
+           odb = ODBFactory.open(fileNameNeodatisDB);
            ArrayList<Object> objs =
                    new ArrayList<Object>(odb.getObjects(ArbolTags.class));
            for (Object o : objs) {
@@ -28,28 +26,19 @@ public class ArbolDAONeodatis extends DAONeodatis<ArbolTags> implements ArbolDAO
 
            }
             odb.store(arbol);
-            odb.close();
        }
        catch (Exception e){
            e.printStackTrace();
        }
+       finally {
+           odb.close();
+        }
 
     }
 
-
     @Override
-    public ArbolTags recuperarArbol() {
-        ODB odb = ODBFactory.open(fileNameNeodatisDB);
-
-        IQuery query = new CriteriaQuery(ArbolTags.class);
-
-        Objects<ArbolTags> objs = odb.getObjects(ArbolTags.class);
-        odb.close();
-
-        if (objs != null && objs.size() != 0)
-            return (ArbolTags) objs.getFirst();
-
-        return new ArbolTags();
+    public ArbolTags recuperar() {
+        return super.obtenerTodos(ArbolTags.class).getFirst();
     }
 
 }
