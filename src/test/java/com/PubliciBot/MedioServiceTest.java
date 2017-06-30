56c09c5b35d105cb712d6e0ce4deacbfc3b5b322
hugo1987s <hugoschreiber1@gmail.com>
2017-06-29 23:40:50 -0300

    Se modifica el metodo de enviarMail, corrigiendo las condiciones de subject. Se corrige el test.

--- src/test/java/com/PubliciBot/MedioServiceTest.java
+++ src/test/java/com/PubliciBot/MedioServiceTest.java
@@ -1,32 +1,33 @@
 package com.PubliciBot;
 
+import com.PubliciBot.DM.AccionPublicitaria;
+import com.PubliciBot.DM.Medio;
+import com.PubliciBot.DM.Mensaje;
+import com.PubliciBot.DM.TipoMedio;
+import com.PubliciBot.Services.AccionPublicitariaService;
+import org.junit.Test;
+
 /**
  * Created by Hugo on 13/06/2017.
  */
 public class MedioServiceTest {
 
-/*
+@Test
     public void enviarMailTest()
     {
         AccionPublicitaria accionPublicitaria=new AccionPublicitaria();
         Medio medio = new Medio();
-        //String path = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
 
-
-        Mensaje mensaje = new Mensaje("Mail enviado desde enviarMailTest!!",  "src/main/resources/test.jpg");
-
+        Mensaje mensaje = new Mensaje("Mail enviado desde enviarMailTest!!",  "src/main/resources/20170629005818698.png");
 
         medio.setTipoMedio(TipoMedio.EMAIL);
 
         AccionPublicitariaService aps=new AccionPublicitariaService();
         AccionPublicitaria AP=new AccionPublicitaria();
 
-        AP.setDestino("agusalexander8@gmail.com");
+        AP.setDestino("megafonomailer@gmail.com");
 
         aps.publicar(AP, mensaje);
-
-
-
     }
-    */
+
 }
